06639723d746309f0978975026004a1b07c8774a
unknown <kummarivijay1@gmail.com>
2022-07-07 13:11:18 +0530

    commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into Project_Manager values ('mohit','1111','pass');
+insert into Project_Manager values ('mayuri','2222','pass1');
+insert into Project_Manager values ('vijayalaxmi','3333','pass2');
+insert into Project_Manager values ('hafiza','4444','pass3');
+insert into Project_Manager values ('vijay','5555','vijay');
